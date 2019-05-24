aa9669f8deabcdd872e2d0d60013a3711d4c28c3
josipx <joze.jenkole@gmail.com>
2019-05-25 00:35:30 +0200

    removed nuget package in csproj

--- Jenx.NuGet.CopyToBin.Host/packages/Jenx.Nuget.CopyToBinDemo.1.0.0.1/tools/Install.ps1
+++
@@ -1,4 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-# change deployed artifact to copy-to-bin
-$project.ProjectItems.Item("ExternalConfig").ProjectItems.Item("CopyToBinConfig.xml").Properties.Item("CopyToOutputDirectory").Value = 1
